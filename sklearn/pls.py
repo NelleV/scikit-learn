46c5254a9fb9b9137fb235af3ff9f39dec34bf6e
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2013-07-22 11:29:20 +0200

    MAINT import pls now raises a DeprecationWarning

--- sklearn/pls.py
+++ sklearn/pls.py
@@ -1,2 +1,7 @@
+import warnings
+
 from CCA.pls import *
 from CCA.cca import CCA
+
+warnings.warn("The pls module was renamed to CCA and will be removed "
+              "in 0.15", DeprecationWarning)
