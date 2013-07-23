aef0a008e313d4c66237a6207f221a1bcd4bcb59
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-07-23 23:15:15 +0200

    API: rename to agglomerative clustering

--- sklearn/cluster/__init__.py
+++ sklearn/cluster/__init__.py
@@ -7,11 +7,13 @@ from .spectral import spectral_clustering, SpectralClustering
 from .mean_shift_ import mean_shift, MeanShift, estimate_bandwidth, \
      get_bin_seeds
 from .affinity_propagation_ import affinity_propagation, AffinityPropagation
-from .hierarchical import ward_tree, Ward, WardAgglomeration
+from .hierarchical import ward_tree, Ward, WardAgglomeration, \
+    AgglomerativeClustering
 from .k_means_ import k_means, KMeans, MiniBatchKMeans
 from .dbscan_ import dbscan, DBSCAN
 
 __all__ = ['AffinityPropagation',
+           'AgglomerativeClustering',
            'DBSCAN',
            'KMeans',
            'MeanShift',
